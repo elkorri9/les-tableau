d3330bee4eb4d6029ee8991895492f29a8275af5
otmane <elkorriotmane@gmail.com>
2024-09-27 10:57:44 +0200

    elkorri

---
+++ exercice01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int T[]={1,2,3,4,5} ;
+    int i ;
+    
+    for (i=0 ; i<5 ; i++){
+        
+        printf("%d\n",T[i]);
+        
+    }
+    
+    
+
+    return 0;
+}
